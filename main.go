4e572b77dea3afbb773250ead7801b6f555b00d9
tnngo <tnngou@gmail.com>
2018-07-18 13:49:23 +0800

    delete test/client.go

--- main.go
+++ main.go
@@ -1,24 +1,4 @@
 package main
 
-import (
-	"github.com/tnngo/nog"
-	"github.com/tnngo/ring/balance"
-)
-
 func main() {
-	b := balance.New()
-	b.SetMode("111", balance.MODE_RR)
-	b.Put("111", "11111")
-	b.Put("111", "11111111111")
-
-	b.Put("222", "22222222")
-
-	//b.SetMode("222", balance.MODE_RR)
-	//b.Push("22222")
-
-	nog.SL.Debug(b.Get("111"))
-	nog.SL.Debug(b.Get("111"))
-	nog.SL.Debug(b.Get("111"))
-	nog.SL.Debug(b.Get("222"))
-	nog.SL.Debugf("111 length: %d", b.Len("111"))
 }
