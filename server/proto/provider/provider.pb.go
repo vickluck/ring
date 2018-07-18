b58ae6035b99fc9a07d71ad9946f775354cf571c
tnngo <tnngou@gmail.com>
2018-07-18 13:46:33 +0800

    new add id

--- server/proto/provider/provider.pb.go
+++ server/proto/provider/provider.pb.go
@@ -37,6 +37,7 @@ type Request struct {
 	Name string `protobuf:"bytes,1,opt,name=name" json:"name,omitempty"`
 	Ip   string `protobuf:"bytes,2,opt,name=ip" json:"ip,omitempty"`
 	Port int32  `protobuf:"varint,3,opt,name=port" json:"port,omitempty"`
+	Id   string `protobuf:"bytes,4,opt,name=id" json:"id,omitempty"`
 }
 
 func (m *Request) Reset()                    { *m = Request{} }
@@ -65,6 +66,13 @@ func (m *Request) GetPort() int32 {
 	return 0
 }
 
+func (m *Request) GetId() string {
+	if m != nil {
+		return m.Id
+	}
+	return ""
+}
+
 type Response struct {
 }
 
@@ -89,7 +97,7 @@ const _ = grpc.SupportPackageIsVersion4
 // Client API for Provider service
 
 type ProviderClient interface {
-	Regist(ctx context.Context, in *Request, opts ...grpc.CallOption) (*Response, error)
+	Registry(ctx context.Context, in *Request, opts ...grpc.CallOption) (*Response, error)
 }
 
 type providerClient struct {
@@ -100,9 +108,9 @@ func NewProviderClient(cc *grpc.ClientConn) ProviderClient {
 	return &providerClient{cc}
 }
 
-func (c *providerClient) Regist(ctx context.Context, in *Request, opts ...grpc.CallOption) (*Response, error) {
+func (c *providerClient) Registry(ctx context.Context, in *Request, opts ...grpc.CallOption) (*Response, error) {
 	out := new(Response)
-	err := grpc.Invoke(ctx, "/provider.Provider/Regist", in, out, c.cc, opts...)
+	err := grpc.Invoke(ctx, "/provider.Provider/Registry", in, out, c.cc, opts...)
 	if err != nil {
 		return nil, err
 	}
@@ -112,27 +120,27 @@ func (c *providerClient) Regist(ctx context.Context, in *Request, opts ...grpc.C
 // Server API for Provider service
 
 type ProviderServer interface {
-	Regist(context.Context, *Request) (*Response, error)
+	Registry(context.Context, *Request) (*Response, error)
 }
 
 func RegisterProviderServer(s *grpc.Server, srv ProviderServer) {
 	s.RegisterService(&_Provider_serviceDesc, srv)
 }
 
-func _Provider_Regist_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+func _Provider_Registry_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
 	in := new(Request)
 	if err := dec(in); err != nil {
 		return nil, err
 	}
 	if interceptor == nil {
-		return srv.(ProviderServer).Regist(ctx, in)
+		return srv.(ProviderServer).Registry(ctx, in)
 	}
 	info := &grpc.UnaryServerInfo{
 		Server:     srv,
-		FullMethod: "/provider.Provider/Regist",
+		FullMethod: "/provider.Provider/Registry",
 	}
 	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(ProviderServer).Regist(ctx, req.(*Request))
+		return srv.(ProviderServer).Registry(ctx, req.(*Request))
 	}
 	return interceptor(ctx, in, info, handler)
 }
@@ -142,8 +150,8 @@ var _Provider_serviceDesc = grpc.ServiceDesc{
 	HandlerType: (*ProviderServer)(nil),
 	Methods: []grpc.MethodDesc{
 		{
-			MethodName: "Regist",
-			Handler:    _Provider_Regist_Handler,
+			MethodName: "Registry",
+			Handler:    _Provider_Registry_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
@@ -153,15 +161,15 @@ var _Provider_serviceDesc = grpc.ServiceDesc{
 func init() { proto.RegisterFile("provider.proto", fileDescriptor0) }
 
 var fileDescriptor0 = []byte{
-	// 149 bytes of a gzipped FileDescriptorProto
+	// 157 bytes of a gzipped FileDescriptorProto
 	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xe2, 0xe2, 0x2b, 0x28, 0xca, 0x2f,
-	0xcb, 0x4c, 0x49, 0x2d, 0xd2, 0x2b, 0x28, 0xca, 0x2f, 0xc9, 0x17, 0xe2, 0x80, 0xf1, 0x95, 0x1c,
+	0xcb, 0x4c, 0x49, 0x2d, 0xd2, 0x2b, 0x28, 0xca, 0x2f, 0xc9, 0x17, 0xe2, 0x80, 0xf1, 0x95, 0x02,
 	0xb9, 0xd8, 0x83, 0x52, 0x0b, 0x4b, 0x53, 0x8b, 0x4b, 0x84, 0x84, 0xb8, 0x58, 0xf2, 0x12, 0x73,
 	0x53, 0x25, 0x18, 0x15, 0x18, 0x35, 0x38, 0x83, 0xc0, 0x6c, 0x21, 0x3e, 0x2e, 0xa6, 0xcc, 0x02,
 	0x09, 0x26, 0xb0, 0x08, 0x53, 0x66, 0x01, 0x48, 0x4d, 0x41, 0x7e, 0x51, 0x89, 0x04, 0xb3, 0x02,
-	0xa3, 0x06, 0x6b, 0x10, 0x98, 0xad, 0xc4, 0xc5, 0xc5, 0x11, 0x94, 0x5a, 0x5c, 0x90, 0x9f, 0x57,
-	0x9c, 0x6a, 0x64, 0xcb, 0xc5, 0x11, 0x00, 0x35, 0x5a, 0xc8, 0x90, 0x8b, 0x2d, 0x28, 0x35, 0x3d,
-	0xb3, 0xb8, 0x44, 0x48, 0x50, 0x0f, 0x6e, 0x3f, 0xd4, 0x32, 0x29, 0x21, 0x64, 0x21, 0x88, 0x66,
-	0x25, 0x86, 0x24, 0x36, 0xb0, 0xf3, 0x8c, 0x01, 0x01, 0x00, 0x00, 0xff, 0xff, 0x48, 0x6f, 0xc5,
-	0xc7, 0xb0, 0x00, 0x00, 0x00,
+	0xa3, 0x06, 0x6b, 0x10, 0x98, 0x0d, 0x56, 0x93, 0x22, 0xc1, 0x02, 0x55, 0x93, 0xa2, 0xc4, 0xc5,
+	0xc5, 0x11, 0x94, 0x5a, 0x5c, 0x90, 0x9f, 0x57, 0x9c, 0x6a, 0x64, 0xcf, 0xc5, 0x11, 0x00, 0xb5,
+	0x4a, 0xc8, 0x18, 0x24, 0x9e, 0x9e, 0x59, 0x5c, 0x52, 0x54, 0x29, 0x24, 0xa8, 0x07, 0x77, 0x11,
+	0xd4, 0x7a, 0x29, 0x21, 0x64, 0x21, 0x88, 0x76, 0x25, 0x86, 0x24, 0x36, 0xb0, 0x83, 0x8d, 0x01,
+	0x01, 0x00, 0x00, 0xff, 0xff, 0x45, 0x8e, 0x6f, 0xbf, 0xc2, 0x00, 0x00, 0x00,
 }
